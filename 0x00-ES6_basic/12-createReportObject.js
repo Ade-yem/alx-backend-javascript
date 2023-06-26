1dde8666276c0a5c19926fda88ed0cc1c1a8faec
Adeyemi Adejumo <93265896+Ade-yem@users.noreply.github.com>
2023-06-26 23:40:14 +0100

    Update 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,4 +1,4 @@
-export default function createReportObject (employeesList) {
+export default function createReportObject(employeesList) {
   const reportObject = {
     allEmployees: {
       ...employeesList
